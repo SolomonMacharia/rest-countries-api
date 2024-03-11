4167c9ac0c57ff4da55621b13d3c52e555fa7877
Solomon Macharia <macha@Solomons-MacBook-Pro.local>
2024-03-11 21:14:55 +0300

    style-country-details-page

--- client/src/pages/Country.tsx
+++ client/src/pages/Country.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 
 import { useCountries } from '../queries/countryData'
 const Country = () => {
@@ -17,21 +18,27 @@ const Country = () => {
   if (loading) return <p>loading...</p>
 
   return (
+	<>
+	<button className='back-btn'>Back</button>
     <div className="country">
-
-        <div key={country[0].name}>
-          <div className="country-flag">
-            <img src={country[0].flags.png} alt={country[0].flags.alt} />
-          </div>
-          <div>
-            <h2>{country[0].nativeName}</h2>
-          </div>
-          <p>{country[0].region}</p>
-          <p>{country[0].subregion}</p>
-          <p>{country[0].population}</p>
-          <p>{country[0].capital}</p>
+        <div className='country__card' key={country[0].name}>
+        	<div className="country__flag">
+        		<img className='country__flag--img' src={country[0].flags.png} alt={country[0].flags.alt} />
+        	</div>
+        	<div className='country__details'>
+            	<h2>{country[0].name}</h2>
+				<p><span className='country__details--span'>Native Name: </span>{country[0].nativeName}</p>
+				<p><span className='country__details--span'>Population: </span>{country[0].population}</p>
+				<p><span className='country__details--span'>Region: </span>{country[0].region}</p>
+				<p><span className='country__details--span'>Sub Region: </span>{country[0].subregion}</p>
+				<p><span className='country__details--span'>Capital: </span>{country[0].capital}</p>
+				<p><span className='country__details--span'>Top Level Domain: </span>{country[0].tld}</p>
+				<p><span className='country__details--span'>Currencies: </span>currencies</p>
+				<p><span className='country__details--span'>Languages: </span>languages</p>
+			</div>
         </div>
     </div>
+	</>
   )
 }
 
